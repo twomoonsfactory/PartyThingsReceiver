2cfb53fc0bf4b9de8505deb839620ea280001db4
twomoonsfactory <twomoonsfactory@gmail.com>
2015-06-07 22:21:08 -0600

    Working to get the UI up

--- js/modules/gameMaster/controllers/gameController.js
+++ js/modules/gameMaster/controllers/gameController.js
@@ -1,9 +1,37 @@
 angular.module('gameMaster')
-	.controller('gameController', ['$scope', '$log', function($scope, $log) {
-	     
-	      	$scope.gamename = null;
-	      	$scope.players = [];
+	.controller('gameController', ['$scope', '$log', '$location', function($scope, $log, $location) {
+	     	$scope.changeView = function(view){
+	     		$location.path(view);
+	     	}
+	      	$scope.gamename = "MyGame";
+	      	$scope.players = [
+	      		{
+	      			playerName: "Billy",
+	      			senderId: 123,
+	      			score: 5,
+	      			state: "writing",
+	      			playerId: 1,
+	      			guessed: false
+	      		},
+	      		{
+	      			playerName: "Bob",
+	      			senderId: 135,
+	      			score: 10,
+	      			state: "ready",
+	      			playerId: 2,
+	      			guessed: false
+	      		},
+	      		{
+	      			playerName: "Samantha",
+	      			senderId: 52412,
+	      			score: 33,
+	      			state: "writing",
+	      			playerId: 3,
+	      			guessed: false
+	      		}
+	      	];
 	      	$scope.things = [];
 	      	$scope.infoDisplay = null;
 	      	$scope.prompts;
+
   	}]);
