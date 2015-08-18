38d1ba277505de5916c4a56a0ba300b409068c96
AJ <aj@twomoonsfactory.com>
2015-08-17 18:30:14 -0600

    working

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -4,19 +4,19 @@ export default ngModule => {
 
     //welcome page
     .when('/welcome', {
-      templateUrl: '../views/welcome.html',
+      templateUrl: '../../views/welcome.html',
       controller: 'welcomeController'
     })
 
     //gameplay page
     .when('/gameplay',{
-      templateUrl: '../views/gameplay.html',
+      templateUrl: '../../views/gameplay.html',
       controller: 'gameController'
     })
 
     //gameend page
     .when('/gameEnd',{
-      templateUrl: '../views/gameEnd.html',
+      templateUrl: '../../views/gameEnd.html',
       controller: 'gameEndController'
     })
 
