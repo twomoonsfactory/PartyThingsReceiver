2b72f4b4e31ffc5adc4ba7ef0604b105b580121b
AJ <aj@twomoonsfactory.com>
2015-10-30 00:49:19 -0600

    missed a reference

--- app/core/vendor.js
+++ app/core/vendor.js
@@ -1,20 +1,12 @@
 module.exports = ()=>{
-	/* JS */
+	//JS
 	require('jQuery');
 	require('angular');
 	require('angular-route');
 	require('underscore');
 	require('angular-material');
 	require('angular-animate');
-	require('../../node_modules/angular-material/angular-material.css');
-	// require('../styles/animation.css');
-	// require('../styles/cards.css');
-	// require('bootstrap');
-	require('../styles/index.scss');
 
-	/* Styles */
-	// require('../index.less');
-	// require('../styles/styling.less');
-	// require('../../node_modules/bootstrap/less/bootstrap.less');
-	//had to comment out a few things in their base style to avoid font load issues
+	//sassy
+	require('../styles/index.scss');
 };
